052e43164865f5952e74fbc45785cd1816e2408d
Delyan Angelov <delian66@gmail.com>
2011-09-13 13:44:48 +0300

    Reference the h2 jar located in the libs folder.

--- h2/h2w.bat
+++ h2/h2w.bat
@@ -1,2 +1,2 @@
-@start javaw -cp "h2-1.3.159.jar;%H2DRIVERS%;%CLASSPATH%" org.h2.tools.Console %*
+@start javaw -cp "../lib/h2-1.3.159.jar;%H2DRIVERS%;%CLASSPATH%" org.h2.tools.Console %*
 @if errorlevel 1 pause
