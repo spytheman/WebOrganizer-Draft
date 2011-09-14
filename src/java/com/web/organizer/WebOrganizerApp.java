3751fa2399bc82310d4e87c4d5ad24bf19980bfc
Delyan Angelov <delian66@gmail.com>
2011-09-14 10:39:37 +0300

    Refactor boilerplate GUI initialization: WebOrganizerApp.

--- src/java/com/web/organizer/WebOrganizerApp.java
+++ src/java/com/web/organizer/WebOrganizerApp.java
@@ -13,6 +13,7 @@ import com.web.organizer.events.BaseEvent.EventType;
 import com.web.organizer.events.EventBus;
 import com.web.organizer.events.OnChangePageListener;
 import com.web.organizer.ui.AboutContent;
+import com.web.organizer.ui.BaseContent;
 import com.web.organizer.ui.ContactContent;
 import com.web.organizer.ui.HelpContent;
 import com.web.organizer.ui.MainContent;
@@ -23,7 +24,6 @@ import com.web.organizer.ui.PolicyContent;
 import com.web.organizer.ui.ShareContent;
 import com.web.organizer.ui.SignUpContent;
 import com.web.organizer.ui.TermsContent;
-import java.util.ArrayList;
 
 /**
  *
@@ -39,92 +39,28 @@ public class WebOrganizerApp extends Application {
     private MainWindow wAbout;
     private MainWindow wPolicy;
     private MainWindow wSignUp;
-    private MainWindow wMainRegisterView;
-    private MainWindow wNewContact;
+    private MainWindow wView;
+    private MainWindow wNContact;
     private EventBus eventBus = new EventBus();
-
-    public void print(Object object) {
-        System.out.println(object.toString());
-
-        /*
-        ArrayList<MainWindow> arr = new ArrayList();
-        arr.add(new MainWindow());
-
-        arr.get(2).setPageContent(wContact);
-         */
-    }
 
     @Override
     public void init() {
-        MainContent mainContent = new MainContent();
-        wMain = new MainWindow(eventBus, mainContent);
-        wMain.setName("Home");
-        setMainWindow(wMain);
-        eventBus.addListener(EventType.CHANGE_PAGE_TO_HOME,
-                new OnChangePageListener(new ExternalResource(wMain.getURL()))
-                );
+        newTab(wMain,     new MainContent(),       "Home",       EventType.CHANGE_PAGE_TO_HOME);
+        newTab(wHelp,     new HelpContent(),       "Help",       EventType.CHANGE_PAGE_TO_HELP);
+        newTab(wTerms,    new TermsContent(),      "Terms",      EventType.CHANGE_PAGE_TO_TERMS);
+        newTab(wAbout,    new AboutContent(),      "About",      EventType.CHANGE_PAGE_TO_ABOUT);
+        newTab(wPolicy,   new PolicyContent(),     "Policy",     EventType.CHANGE_PAGE_TO_POLICY);
+        newTab(wSignUp,   new SignUpContent(),     "SignUp",     EventType.CHANGE_PAGE_TO_SIGNUP);
+        newTab(wShare,    new ShareContent(),      "Share",      EventType.CHANGE_PAGE_TO_SHARE);
+        newTab(wContact,  new ContactContent(),    "Contact",    EventType.CHANGE_PAGE_TO_CONTACT);
+        newTab(wView,     new MainViewContent(),   "Register",   EventType.CHANGE_PAGE_TO_MAINVIEW);
+        newTab(wNContact, new NewContactContent(), "NewContact", EventType.CHANGE_PAGE_TO_NEWCONTACT);
+    }
 
-        wHelp = new MainWindow(eventBus, new HelpContent());
-        wHelp.setName("help");
-        addWindow(wHelp);
-        eventBus.addListener(EventType.CHANGE_PAGE_TO_HELP,
-                new OnChangePageListener(new ExternalResource(wHelp.getURL()))
-                );
-
-        wTerms = new MainWindow(eventBus, new TermsContent());
-        wTerms.setName("terms");
-        addWindow(wTerms);
-        eventBus.addListener(EventType.CHANGE_PAGE_TO_TERMS,
-                new OnChangePageListener(new ExternalResource(wTerms.getURL()))
-                );
-
-        wAbout = new MainWindow(eventBus, new AboutContent());
-        wAbout.setName("about");
-        addWindow(wAbout);
-        eventBus.addListener(EventType.CHANGE_PAGE_TO_ABOUT,
-                new OnChangePageListener(new ExternalResource(wAbout.getURL()))
-                );
-
-        wPolicy = new MainWindow(eventBus, new PolicyContent());
-        wPolicy.setName("policy");
-        addWindow(wPolicy);
-        eventBus.addListener(EventType.CHANGE_PAGE_TO_POLICY,
-                new OnChangePageListener(new ExternalResource(wPolicy.getURL()))
-                );
-
-        wSignUp = new MainWindow(eventBus, new SignUpContent());
-        wSignUp.setName("signUp");
-        addWindow(wSignUp);
-        eventBus.addListener(EventType.CHANGE_PAGE_TO_SIGNUP,
-                new OnChangePageListener(new ExternalResource(wSignUp.getURL()))
-                );
-
-        wShare = new MainWindow(eventBus, new ShareContent());
-        wShare.setName("share");
-        addWindow(wShare);
-        eventBus.addListener(EventType.CHANGE_PAGE_TO_SHARE,
-                new OnChangePageListener(new ExternalResource(wShare.getURL()))
-                );
-
-        wContact = new MainWindow(eventBus, new ContactContent());
-        wContact.setName("contact");
-        addWindow(wContact);
-        eventBus.addListener(EventType.CHANGE_PAGE_TO_CONTACT,
-                new OnChangePageListener(new ExternalResource(wContact.getURL()))
-                );
-
-        wMainRegisterView = new MainWindow(eventBus, new MainViewContent());
-        wMainRegisterView.setName("registerview");
-        addWindow(wMainRegisterView);
-        eventBus.addListener(EventType.CHANGE_PAGE_TO_MAINVIEW,
-                new OnChangePageListener(new ExternalResource(wMainRegisterView.getURL()))
-                );
-
-        wNewContact = new MainWindow(eventBus, new NewContactContent());
-        wNewContact.setName("newcontact");
-        addWindow(wNewContact);
-        eventBus.addListener(EventType.CHANGE_PAGE_TO_NEWCONTACT,
-                new OnChangePageListener(new ExternalResource(wNewContact.getURL()))
-                );
+    private void newTab(MainWindow tab, BaseContent c, String label, EventType e){
+        tab = new MainWindow(eventBus, c);
+        tab.setName(label);
+        setMainWindow(tab);
+        eventBus.addListener(e,  new OnChangePageListener(new ExternalResource(tab.getURL())) );
     }
 }
