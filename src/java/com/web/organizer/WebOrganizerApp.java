b64c6770e09bbfd6c327ba1082a8a2c5f22b223c
Delyan Angelov <delian66@gmail.com>
2011-09-14 11:16:17 +0300

    Reduce the cruft ... the private application window variables are not used anywhere.

--- src/java/com/web/organizer/WebOrganizerApp.java
+++ src/java/com/web/organizer/WebOrganizerApp.java
@@ -29,37 +29,28 @@ import com.web.organizer.ui.TermsContent;
  * @author Gery
  */
 public class WebOrganizerApp extends Application {
-
-    private MainWindow wMain;
-    private MainWindow wHelp;
-    private MainWindow wShare;
-    private MainWindow wContact;
-    private MainWindow wTerms;
-    private MainWindow wAbout;
-    private MainWindow wPolicy;
-    private MainWindow wSignUp;
-    private MainWindow wView;
-    private MainWindow wNContact;
     private EventBus eventBus;
 
     @Override
     public void init() {
         eventBus = new EventBus();
 
-        newTab(wMain, new MainContent(), "Home", EventType.CHANGE_PAGE_TO_HOME);
-        newTab(wHelp, new HelpContent(), "Help", EventType.CHANGE_PAGE_TO_HELP);
-        newTab(wTerms, new TermsContent(), "Terms", EventType.CHANGE_PAGE_TO_TERMS);
-        newTab(wAbout, new AboutContent(), "About", EventType.CHANGE_PAGE_TO_ABOUT);
-        newTab(wPolicy, new PolicyContent(), "Policy", EventType.CHANGE_PAGE_TO_POLICY);
-        newTab(wSignUp, new SignUpContent(), "SignUp", EventType.CHANGE_PAGE_TO_SIGNUP);
-        newTab(wShare, new ShareContent(), "Share", EventType.CHANGE_PAGE_TO_SHARE);
-        newTab(wContact, new ContactContent(), "Contact", EventType.CHANGE_PAGE_TO_CONTACT);
-        newTab(wView, new MainViewContent(), "Register", EventType.CHANGE_PAGE_TO_MAINVIEW);
-        newTab(wNContact, new NewContactContent(), "NewContact", EventType.CHANGE_PAGE_TO_NEWCONTACT);
+        newTab(new HelpContent(), "Help", EventType.CHANGE_PAGE_TO_HELP);
+        newTab(new TermsContent(), "Terms", EventType.CHANGE_PAGE_TO_TERMS);
+        newTab(new AboutContent(), "About", EventType.CHANGE_PAGE_TO_ABOUT);
+        newTab(new PolicyContent(), "Policy", EventType.CHANGE_PAGE_TO_POLICY);
+        newTab(new SignUpContent(), "SignUp", EventType.CHANGE_PAGE_TO_SIGNUP);
+        newTab(new ShareContent(), "Share", EventType.CHANGE_PAGE_TO_SHARE);
+        newTab(new ContactContent(), "Contact", EventType.CHANGE_PAGE_TO_CONTACT);
+        newTab(new MainViewContent(), "Register", EventType.CHANGE_PAGE_TO_MAINVIEW);
+        newTab(new NewContactContent(), "NewContact", EventType.CHANGE_PAGE_TO_NEWCONTACT);
+        
+        // The last one will be the default view
+        newTab(new MainContent(), "/", EventType.CHANGE_PAGE_TO_HOME);
     }
 
-    private void newTab(MainWindow tab, BaseContent c, String label, EventType e) {
-        tab = new MainWindow(eventBus, c);
+    private void newTab(BaseContent c, String label, EventType e) {
+        MainWindow tab = new MainWindow(eventBus, c);
         tab.setName(label);
         setMainWindow(tab);
         eventBus.addListener(e, new OnChangePageListener(new ExternalResource(tab.getURL())));
